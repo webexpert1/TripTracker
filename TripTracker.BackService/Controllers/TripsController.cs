130b95eb8f51734166e0d8f74467e5ff7d1aad01
Richard Okoye <emmanuel.eze@netopng.com>
2020-01-05 19:59:30 +0100

    Add project files.

---
+++ TripTracker.BackService/Controllers/TripsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TripTracker.BackService.Models;
+
+namespace TripTracker.BackService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Trips")]
+    public class TripsController : Controller
+    {
+        private readonly Repository _repository;
+        public TripsController(Repository repository)
+        {
+            _repository = repository;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Trip> Get()
+        {
+            return _repository.Get();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
